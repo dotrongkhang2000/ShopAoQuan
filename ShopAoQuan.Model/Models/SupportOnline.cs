0de358993758cba232138163e35ccd4e12d04d1e
dtKhang <dotrongkhang2000@gmail.com>
2021-03-23 16:01:54 +0700

    Uncommited Changes

---
+++ ShopAoQuan.Model/Models/SupportOnline.cs
@@ -0,0 +1,30 @@
+namespace ShopAoQuan.Model.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class SupportOnline
+    {
+        public int ID { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Skype { get; set; }
+
+        [StringLength(50)]
+        public string Moblie { get; set; }
+
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [StringLength(250)]
+        public string Facebook { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
